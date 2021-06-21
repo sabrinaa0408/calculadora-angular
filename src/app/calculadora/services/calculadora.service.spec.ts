a446962bb0111b7f791a59558f27903cc7f0f671
sabrinaa0408 <sabrina.silva16@outlook.com>
2021-06-21 18:44:53 -0300

    finalizando calculadoa em angular

---
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { CalculadoraService } from './calculadora.service';
+
+describe('CalculadoraService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [CalculadoraService]
+  }));
+
+  it('should be created', () => {
+    const service: CalculadoraService = TestBed.get(CalculadoraService);
+    expect(service).toBeTruthy();
+  });
+
+  it('deve garantir que 1 + 4 = 5', 
+  inject([CalculadoraService], (service: CalculadoraService) =>{
+    let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+    expect(soma).toEqual(5);
+  }))
+});
